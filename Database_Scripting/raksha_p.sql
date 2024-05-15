3edd6a0f9a8fc00a1a948fd61ea2cc7d18bf15eb
Ruchimishra1996 <162128403+Ruchimishra1996@users.noreply.github.com>
2024-05-15 21:32:30 +0530

    Add files via upload

---
+++ Database_Scripting/raksha_p.sql
@@ -0,0 +1,12 @@
+Create database raksha_p;
+
+Use  raksha_p;
+
+select * from categories;
+select * from customers;
+select * from orderdetails;
+select * from orders;
+select * from payments;
+select * from products;
+select * from shippers;
+select * from suppliers;
